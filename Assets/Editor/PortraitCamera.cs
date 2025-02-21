8d3648ba576e96c9f78fcf65537743603388d103
Macchiato <simppp@gmail.com>
2025-02-21 15:46:40 +0900

    VRSuya.Core 의존성으로 변경

--- Assets/Editor/PortraitCamera.cs
+++ Assets/Editor/PortraitCamera.cs
@@ -1,6 +1,5 @@
 ﻿#if UNITY_EDITOR
 using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
 
 using UnityEditor;
@@ -8,6 +7,9 @@ using UnityEngine;
 using UnityEngine.UIElements;
 
 using VRC.SDKBase;
+
+using static VRSuya.Core.Unity;
+using Avatar = VRSuya.Core.Avatar;
 
 /*
  * VRSuya Utility
@@ -241,7 +243,8 @@ namespace com.vrsuya.utility {
 
 		/// <summary>지정된 색상으로 아바타 프로필용 카메라를 생성합니다.</summary>
 		private static Camera AddNewCamera(string HEXColorCode) {
-			VRC_AvatarDescriptor TargetAvatarDescriptor = GetVRCAvatar();
+			Avatar AvatarInstance = new Avatar();
+			VRC_AvatarDescriptor TargetAvatarDescriptor = AvatarInstance.GetVRCAvatarDescriptor();
 			if (TargetAvatarDescriptor) {
 				GameObject newGameObject = new GameObject("PortraitCamera");
 				Camera newCameraComponent = newGameObject.AddComponent<Camera>();
@@ -280,69 +283,6 @@ namespace com.vrsuya.utility {
 			Quaternion ReferenceRotation = Quaternion.Euler(0, 180, 0);
 			Quaternion RotationDifference = AvatarRotation * Quaternion.Inverse(ReferenceRotation);
 			return RotationDifference;
-		}
-
-		/// <summary>Scene에서 조건에 맞는 VRC AvatarDescriptor 컴포넌트 아바타 1개를 반환합니다.</summary>
-		/// <returns>조건에 맞는 VRC 아바타</returns>
-		private static VRC_AvatarDescriptor GetVRCAvatar() {
-			VRC_AvatarDescriptor TargetAvatarDescriptor = GetAvatarDescriptorFromVRCSDKBuilder();
-			if (!TargetAvatarDescriptor) TargetAvatarDescriptor = GetAvatarDescriptorFromSelection();
-			if (!TargetAvatarDescriptor) TargetAvatarDescriptor = GetAvatarDescriptorFromVRCTool();
-			return TargetAvatarDescriptor;
-		}
-
-		/// <summary>VRCSDK Builder에서 활성화 상태인 VRC 아바타를 반환합니다.</summary>
-		/// <returns>VRCSDK Builder에서 활성화 상태인 VRC 아바타</returns>
-		private static VRC_AvatarDescriptor GetAvatarDescriptorFromVRCSDKBuilder() {
-			return null;
-		}
-
-		/// <summary>Unity 하이어라키에서 선택한 GameObject 중에서 VRC AvatarDescriptor 컴포넌트가 존재하는 아바타를 1개를 반환합니다.</summary>
-		/// <returns>선택 중인 VRC 아바타</returns>
-		private static VRC_AvatarDescriptor GetAvatarDescriptorFromSelection() {
-			GameObject[] SelectedGameObjects = Selection.gameObjects;
-			if (SelectedGameObjects.Length == 1) {
-				VRC_AvatarDescriptor SelectedVRCAvatarDescriptor = SelectedGameObjects[0].GetComponent<VRC_AvatarDescriptor>();
-				if (SelectedVRCAvatarDescriptor) {
-					return SelectedVRCAvatarDescriptor;
-				} else {
-					return null;
-				}
-			} else if (SelectedGameObjects.Length > 1) {
-				VRC_AvatarDescriptor SelectedVRCAvatarDescriptor = SelectedGameObjects
-					.Where(SelectedGameObject => SelectedGameObject.activeInHierarchy == true)
-					.Select(SelectedGameObject => SelectedGameObject.GetComponent<VRC_AvatarDescriptor>()).ToArray()[0];
-				if (SelectedVRCAvatarDescriptor) {
-					return SelectedVRCAvatarDescriptor;
-				} else {
-					return null;
-				}
-			} else {
-				return null;
-			}
-		}
-
-		/// <summary>Scene에서 활성화 상태인 VRC AvatarDescriptor 컴포넌트가 존재하는 아바타를 1개를 반환합니다.</summary>
-		/// <returns>Scene에서 활성화 상태인 VRC 아바타</returns>
-		private static VRC_AvatarDescriptor GetAvatarDescriptorFromVRCTool() {
-			VRC_AvatarDescriptor[] AllVRCAvatarDescriptor = VRC.Tools.FindSceneObjectsOfTypeAll<VRC_AvatarDescriptor>().ToArray();
-			if (AllVRCAvatarDescriptor.Length > 0) {
-				return AllVRCAvatarDescriptor.Where(Avatar => Avatar.gameObject.activeInHierarchy).ToArray()[0];
-			} else {
-				return null;
-			}
-		}
-
-		/// <summary>HEX 문자열을 Color로 변환합니다.</summary>
-		/// <param name="HEXColorCode">변환할 HEX 문자열 (예: #RRGGBB)</param>
-		/// <returns>변환된 Color 객체</returns>
-		private static Color HexToColor(string HEXColorCode) {
-			if (HEXColorCode.StartsWith("#")) HEXColorCode = HEXColorCode.Substring(1);
-			if (HEXColorCode.Length != 6 ) return Color.black;
-			float r = int.Parse(HEXColorCode.Substring(0, 2), System.Globalization.NumberStyles.HexNumber) / 255f;
-			float g = int.Parse(HEXColorCode.Substring(2, 2), System.Globalization.NumberStyles.HexNumber) / 255f;
-			float b = int.Parse(HEXColorCode.Substring(4, 2), System.Globalization.NumberStyles.HexNumber) / 255f;
-			return new Color(r, g, b);
 		}
 
 		private static void ApplyCustomCameraSettings(Camera TargetCamera) {
