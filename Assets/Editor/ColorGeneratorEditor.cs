eaef5eab6c329bfefe60be1ffb4243811359e1ca
Macchiato <simppp@gmail.com>
2025-03-14 22:17:39 +0900

    쉐이딩 컬러 생성 프로그램 초안 작성

---
+++ Assets/Editor/ColorGeneratorEditor.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/*
+ * VRSuya Utility
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.utility {
+
+	public class ColorGeneratorEditor : EditorWindow {
+
+		ColorGenerator ColorGeneratorInstance;
+		SerializedObject SerializedColorGenerator;
+		SerializedProperty SerializedShadeColor1;
+		SerializedProperty SerializedShadeColor2;
+		SerializedProperty SerializedShadeColor3;
+		SerializedProperty SerializedShadeColor4;
+		SerializedProperty SerializedRimLightColor;
+		SerializedProperty SerializedRimShadowColor;
+		SerializedProperty SerializedTargetMaterial;
+
+		// ColorBox Rect 변수
+		private float BorderX = 30f;
+		private float BorderY = 30f;
+		private float ShadowRectWidth = float.NaN;
+		private float ShadeRectWidth = float.NaN;
+		private float RectHeight = 100f;
+		private float ColorFieldOffset = 2f;
+		private Rect ShadeBoxPosition1 = new Rect();
+		private Rect ShadeBoxPosition2 = new Rect();
+		private Rect ShadeBoxPosition3 = new Rect();
+		private Rect ShadeBoxPosition4 = new Rect();
+		private Rect RimLightBoxPosition = new Rect();
+		private Rect RimShadeBoxPosition = new Rect();
+
+		void OnEnable() {
+			ColorGeneratorInstance = CreateInstance<ColorGenerator>();
+			SerializedColorGenerator = new SerializedObject(ColorGeneratorInstance);
+			SerializedShadeColor1 = SerializedColorGenerator.FindProperty("ShadeColor1");
+			SerializedShadeColor2 = SerializedColorGenerator.FindProperty("ShadeColor2");
+			SerializedShadeColor3 = SerializedColorGenerator.FindProperty("ShadeColor3");
+			SerializedShadeColor4 = SerializedColorGenerator.FindProperty("ShadeColor4");
+			SerializedRimLightColor = SerializedColorGenerator.FindProperty("RimLightColor");
+			SerializedRimShadowColor = SerializedColorGenerator.FindProperty("RimShadeColor");
+			SerializedTargetMaterial = SerializedColorGenerator.FindProperty("TargetMaterial");
+			return;
+		}
+
+		[MenuItem("Tools/VRSuya/ColorGenerator", priority = 1000)]
+		static void CreateWindow() {
+			ColorGeneratorEditor AppWindow = (ColorGeneratorEditor)GetWindow(typeof(ColorGeneratorEditor), true, "ColorGenerator", true);
+			return;
+		}
+
+		void OnGUI() {
+			if (ColorGeneratorInstance == null) {
+				Close();
+				return;
+			}
+			SerializedColorGenerator.Update();
+			Vector2 WindowSize = position.size;
+			GUIStyle CenteredStyle = new GUIStyle(EditorStyles.label) { alignment = TextAnchor.MiddleCenter };
+			(ShadeBoxPosition1, ShadeBoxPosition2, ShadeBoxPosition3, ShadeBoxPosition4) = GetShadeBoxPosition(WindowSize);
+			(RimLightBoxPosition, RimShadeBoxPosition) = GetRimShadeBoxPosition(WindowSize);
+			GUILayout.Space(BorderY + RectHeight + (RectHeight / 4) + EditorGUIUtility.singleLineHeight);
+			Rect ShadeColorBox1 = ShadeBoxPosition1;
+			Rect ShadeColorBox2 = ShadeBoxPosition2;
+			Rect ShadeColorBox3 = ShadeBoxPosition3;
+			Rect ShadeColorBox4 = ShadeBoxPosition4;
+			Rect RimLightColorBox = RimLightBoxPosition;
+			Rect RimShadeColorBox = RimShadeBoxPosition;
+			EditorGUI.DrawRect(ShadeColorBox1, SerializedShadeColor1.colorValue);
+			EditorGUI.DrawRect(ShadeColorBox2, SerializedShadeColor2.colorValue);
+			EditorGUI.DrawRect(ShadeColorBox3, SerializedShadeColor3.colorValue);
+			EditorGUI.DrawRect(ShadeColorBox4, SerializedShadeColor4.colorValue);
+			EditorGUI.DrawRect(RimLightColorBox, SerializedRimLightColor.colorValue);
+			EditorGUI.DrawRect(RimShadeColorBox, SerializedRimShadowColor.colorValue);
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(BorderX);
+			EditorGUILayout.PropertyField(SerializedShadeColor1, new GUIContent(string.Empty), GUILayout.Width(ShadowRectWidth - ColorFieldOffset));
+			EditorGUILayout.PropertyField(SerializedShadeColor2, new GUIContent(string.Empty), GUILayout.Width(ShadowRectWidth - ColorFieldOffset));
+			EditorGUILayout.PropertyField(SerializedShadeColor3, new GUIContent(string.Empty), GUILayout.Width(ShadowRectWidth - ColorFieldOffset));
+			EditorGUILayout.PropertyField(SerializedShadeColor4, new GUIContent(string.Empty), GUILayout.Width(ShadowRectWidth - ColorFieldOffset));
+			GUILayout.Space(BorderX);
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(BorderX);
+			EditorGUILayout.PropertyField(SerializedRimLightColor, new GUIContent(string.Empty), GUILayout.Width(ShadeRectWidth - (ColorFieldOffset / 2)));
+			EditorGUILayout.PropertyField(SerializedRimShadowColor, new GUIContent(string.Empty), GUILayout.Width(ShadeRectWidth - (ColorFieldOffset / 2)));
+			GUILayout.Space(BorderX);
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(BorderX);
+			if (GUILayout.Button("재계산", GUILayout.Width(ShadowRectWidth - ColorFieldOffset))) {
+				ColorGeneratorInstance.ModifiedColor1();
+			}
+			if (GUILayout.Button("재계산", GUILayout.Width(ShadowRectWidth - ColorFieldOffset))) {
+				ColorGeneratorInstance.ModifiedColor2();
+			}
+			if (GUILayout.Button("재계산", GUILayout.Width(ShadowRectWidth - ColorFieldOffset))) {
+				ColorGeneratorInstance.ModifiedColor3();
+			}
+			if (GUILayout.Button("재계산", GUILayout.Width(ShadowRectWidth - ColorFieldOffset))) {
+				ColorGeneratorInstance.ModifiedColor4();
+			}
+			GUILayout.Space(BorderX);
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(BorderX);
+			GUILayout.Label("베이스 컬러", CenteredStyle, GUILayout.Width(ShadowRectWidth));
+			GUILayout.Label("1단 그림자", CenteredStyle, GUILayout.Width(ShadowRectWidth));
+			GUILayout.Label("2단 그림자", CenteredStyle, GUILayout.Width(ShadowRectWidth));
+			GUILayout.Label("3단 그림자", CenteredStyle, GUILayout.Width(ShadowRectWidth));
+			GUILayout.Space(BorderX);
+			GUILayout.EndHorizontal();
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(BorderX);
+			GUILayout.Label("림라이트 컬러", CenteredStyle, GUILayout.Width(ShadeRectWidth));
+			GUILayout.Label("림쉐도우 컬러", CenteredStyle, GUILayout.Width(ShadeRectWidth));
+			GUILayout.Space(BorderX);
+			GUILayout.EndHorizontal();
+			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			GUILayout.BeginHorizontal();
+			GUILayout.Space(BorderX);
+			GUILayout.FlexibleSpace();
+			EditorGUILayout.PropertyField(SerializedTargetMaterial, new GUIContent(string.Empty));
+			GUILayout.Space(10);
+			if (GUILayout.Button("추출", GUILayout.Width(100))) {
+				ColorGeneratorInstance.RequestGetMaterialShadeColor();
+			}
+			GUILayout.Space(10);
+			if (GUILayout.Button("적용", GUILayout.Width(100))) {
+				ColorGeneratorInstance.RequestSetMaterialShadeColor();
+				Repaint();
+			}
+			GUILayout.Space(10);
+			if (GUILayout.Button("실행 취소", GUILayout.Width(100))) {
+				ColorGeneratorInstance.DebugColorDelta();
+				// Undo.PerformUndo();
+				// Repaint();
+			}
+			GUILayout.FlexibleSpace();
+			GUILayout.Space(BorderX);
+			GUILayout.EndHorizontal();
+			EditorGUILayout.Space(EditorGUIUtility.singleLineHeight);
+			UpdateModifiedColor();
+			SerializedColorGenerator.ApplyModifiedProperties();
+			return;
+		}
+
+		private (Rect, Rect, Rect, Rect) GetShadeBoxPosition(Vector2 CurrentWindowSize) {
+			ShadowRectWidth = (CurrentWindowSize.x - BorderX * 2) / 4;
+			Rect NewShadeBoxPosition1 = new Rect(BorderX + ShadowRectWidth * 0, BorderY, ShadowRectWidth, RectHeight);
+			Rect NewShadeBoxPosition2 = new Rect(BorderX + ShadowRectWidth * 1, BorderY, ShadowRectWidth, RectHeight);
+			Rect NewShadeBoxPosition3 = new Rect(BorderX + ShadowRectWidth * 2, BorderY, ShadowRectWidth, RectHeight);
+			Rect NewShadeBoxPosition4 = new Rect(BorderX + ShadowRectWidth * 3, BorderY, ShadowRectWidth, RectHeight);
+			return (NewShadeBoxPosition1, NewShadeBoxPosition2, NewShadeBoxPosition3, NewShadeBoxPosition4);
+		}
+
+		private (Rect, Rect) GetRimShadeBoxPosition(Vector2 CurrentWindowSize) {
+			ShadeRectWidth = (CurrentWindowSize.x - BorderX * 2) / 2;
+			Rect NewRimLightBoxPosition = new Rect(BorderX + ShadeRectWidth * 0, BorderY + RectHeight, ShadeRectWidth, RectHeight / 4);
+			Rect NewRimShadeBoxPosition = new Rect(BorderX + ShadeRectWidth * 1, BorderY + RectHeight, ShadeRectWidth, RectHeight / 4);
+			return (NewRimLightBoxPosition, NewRimShadeBoxPosition);
+		}
+
+		private void UpdateModifiedColor() {
+			if (SerializedColorGenerator.hasModifiedProperties) {
+				if (SerializedShadeColor1.prefabOverride) {
+					ColorGeneratorInstance.ModifiedColor1();
+					Debug.Log("ShadeColor1이 변경되었습니다.");
+				}
+				if (SerializedShadeColor2.prefabOverride) {
+					ColorGeneratorInstance.ModifiedColor2();
+					Debug.Log("ShadeColor2이 변경되었습니다.");
+				}
+				if (SerializedShadeColor3.prefabOverride) {
+					ColorGeneratorInstance.ModifiedColor3();
+					Debug.Log("ShadeColor3이 변경되었습니다.");
+				}
+				if (SerializedShadeColor4.prefabOverride) {
+					ColorGeneratorInstance.ModifiedColor4();
+					Debug.Log("ShadeColor4이 변경되었습니다.");
+				}
+			}
+			return;
+		}
+	}
+}
---
+++ Assets/Runtime/ColorGenerator.cs
@@ -0,0 +1,208 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEditor;
+
+using static VRSuya.Core.Unity;
+
+/*
+ * VRSuya Utility
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.utility {
+
+	public class ColorGenerator : ScriptableObject {
+
+		[Serializable]
+		public struct ColorDelta {
+			public string Name_EN;
+			public string Name_KO;
+			public string Name_JA;
+			public ShadeType TargetShade;
+			public string ReferenceColor;
+			public Vector3 ColorDelta1;
+			public Vector3 ColorDelta2;
+			public Vector3 ColorDelta3;
+			public Vector3 RimLightDelta;
+			public Vector3 RimShadeDelta;
+		}
+
+		public enum ShadeType {
+			Body, Hair, Cloth
+		}
+
+		private static List<ColorDelta> ColorDeltaList = new List<ColorDelta>() {
+			new ColorDelta {
+				Name_EN = "Glossy",
+				Name_KO = "윤광",
+				Name_JA = "グロッシー",
+				TargetShade = ShadeType.Body,
+				ReferenceColor = "#FFF0EF",
+				ColorDelta1 = new Vector3(349f, 4f, -9f),
+				ColorDelta2 = new Vector3(-4f, 6f, 1f),
+				ColorDelta3 = new Vector3(7f, 16f, -3f),
+				RimLightDelta = new Vector3(-348f, 13f, 9f),
+				RimShadeDelta = new Vector3(-4f, -22f, 2f)
+			}
+		};
+		private ColorDelta TargetColorDelta = ColorDeltaList[0];
+
+		public Color ShadeColor1;
+		public Color ShadeColor2;
+		public Color ShadeColor3;
+		public Color ShadeColor4;
+		public Color RimLightColor;
+		public Color RimShadeColor;
+
+		public Material TargetMaterial;
+
+		// Unity Undo 변수
+		private readonly string UndoGroupName = "VRSuya ColorGenerator";
+		private int UndoGroupIndex;
+
+		private void OnEnable() {
+			ShadeColor1 = HexToColor(TargetColorDelta.ReferenceColor);
+			ShadeColor2 = GetDeltaColor(ShadeColor1, TargetColorDelta.ColorDelta1, false);
+			ShadeColor3 = GetDeltaColor(ShadeColor2, TargetColorDelta.ColorDelta2, false);
+			ShadeColor4 = GetDeltaColor(ShadeColor3, TargetColorDelta.ColorDelta3, false);
+			RimLightColor = GetDeltaColor(ShadeColor2, TargetColorDelta.RimLightDelta, false);
+			RimShadeColor = GetDeltaColor(ShadeColor4, TargetColorDelta.RimShadeDelta, false);
+			return;
+		}
+
+		public void DebugColorDelta() {
+			Vector3 Delta1 = GetHSVColorDelta(ShadeColor1, ShadeColor2);
+			Vector3 Delta2 = GetHSVColorDelta(ShadeColor2, ShadeColor3);
+			Vector3 Delta3 = GetHSVColorDelta(ShadeColor3, ShadeColor4);
+			Vector3 RimLightDelta = GetHSVColorDelta(ShadeColor2, HexToColor("FFC8C3"));
+			Vector3 RimShadeDelta = GetHSVColorDelta(ShadeColor4, HexToColor("E9D1D4"));
+			Debug.Log(Delta1);
+			Debug.Log(Delta2);
+			Debug.Log(Delta3);
+			Debug.Log(RimLightDelta);
+			Debug.Log(RimShadeDelta);
+		}
+
+		public void RequestGetMaterialShadeColor() {
+			if (TargetMaterial) {
+				if (GetShaderType(TargetMaterial) == "lilToon") {
+					if (TargetMaterial.GetFloat("_UseShadow") == 1f) {
+						ShadeColor1 = Color.white;
+						ShadeColor2 = TargetMaterial.GetColor("_ShadowColor");
+						if (TargetMaterial.GetColor("_Shadow2ndColor").a != 0f) {
+							ShadeColor3 = TargetMaterial.GetColor("_Shadow2ndColor");
+						} else {
+							ShadeColor3 = ShadeColor2;
+						}
+						if (TargetMaterial.GetColor("_Shadow3rdColor").a != 0f) {
+							ShadeColor4 = TargetMaterial.GetColor("_Shadow3rdColor");
+						} else {
+							ShadeColor4 = ShadeColor3;
+						}
+					} else {
+						ShadeColor1 = Color.white;
+						ShadeColor2 = Color.black;
+						ShadeColor3 = Color.black;
+						ShadeColor4 = Color.black;
+					}
+				}
+			}
+			return;
+		}
+
+		public void RequestSetMaterialShadeColor() {
+			if (TargetMaterial) {
+				if (GetShaderType(TargetMaterial) == "lilToon") {
+					UndoGroupIndex = InitializeUndoGroup(UndoGroupName);
+					Undo.RecordObject(TargetMaterial, UndoGroupName);
+					if (ShadeColor2 != Color.black) TargetMaterial.SetColor("_ShadowColor", ShadeColor2);
+					if (ShadeColor3 != Color.black) TargetMaterial.SetColor("_Shadow2ndColor", ShadeColor3);
+					if (ShadeColor4 != Color.black) TargetMaterial.SetColor("_Shadow3rdColor", ShadeColor4);
+					EditorUtility.SetDirty(TargetMaterial);
+					Undo.CollapseUndoOperations(UndoGroupIndex);
+					Debug.Log($"[ColorGenerator] {TargetMaterial.name} 머테리얼에 설정을 적용하였습니다.");
+				}
+			}
+			return;
+		}
+
+		private Vector3 GetHSVColorDelta(Color OriginalColor, Color TargetColor) {
+			Vector3 OriginalHSV = ConvertRGBToHSV(OriginalColor);
+			Vector3 TargetHSV = ConvertRGBToHSV(TargetColor);
+			float DeltaH = Mathf.Round((TargetHSV.x - OriginalHSV.x) * 360);
+			float DeltaS = Mathf.Round((TargetHSV.y - OriginalHSV.y) * 100);
+			float DeltaV = Mathf.Round((TargetHSV.z - OriginalHSV.z) * 100);
+			return new Vector3(DeltaH, DeltaS, DeltaV);
+		}
+
+		private static Vector3 ConvertHSVtoUnityHSV(Vector3 TargetHSV) {
+			return new Vector3(TargetHSV.x / 360, TargetHSV.y / 100, TargetHSV.z / 100);
+		}
+
+		private static Color GetDeltaColor(Color TargetColor, Vector3 TargetDelta, bool Backward) {
+			Vector3 TargetHSV = ConvertRGBToHSV(TargetColor);
+			Vector3 UnityDeltaHSV = ConvertHSVtoUnityHSV(TargetDelta);
+			float NewH = (!Backward) ? TargetHSV.x + UnityDeltaHSV.x : TargetHSV.x - UnityDeltaHSV.x;
+			float NewS = (!Backward) ? TargetHSV.y + UnityDeltaHSV.y : TargetHSV.y - UnityDeltaHSV.y;
+			float NewV = (!Backward) ? TargetHSV.z + UnityDeltaHSV.z : TargetHSV.z - UnityDeltaHSV.z;
+			if (NewH > 1f) {
+				NewH = 1f;
+			} else if (NewH < 0f) {
+				NewH = 0f;
+			}
+			if (NewS > 1f) {
+				NewS = 1f;
+			} else if (NewS < 0f) {
+				NewS = 0f;
+			}
+			if (NewV > 1f) {
+				NewV = 1f;
+			} else if (NewV < 0f) {
+				NewV = 0f;
+			}
+			return Color.HSVToRGB(NewH, NewS, NewV);
+		}
+
+		/// <summary>해당 머테리얼이 어떠한 쉐이더를 사용하는지 String으로 반환합니다.</summary>
+		/// <returns>머테리얼이 사용하고 있는 쉐이더</returns>
+		private static string GetShaderType(Material TargetMaterial) {
+			string ShaderType = TargetMaterial.shader.name;
+			if (TargetMaterial.shader.name.Contains("lilToon")) ShaderType = "lilToon";
+			if (TargetMaterial.shader.name.Contains("poiyomi")) ShaderType = "poiyomi";
+			if (TargetMaterial.shader.name.Contains("UnityChanToonShader")) ShaderType = "UnityChanToonShader";
+			return ShaderType;
+		}
+
+		public void ModifiedColor1() {
+			ShadeColor2 = GetDeltaColor(ShadeColor1, TargetColorDelta.ColorDelta1, false);
+			ShadeColor3 = GetDeltaColor(ShadeColor2, TargetColorDelta.ColorDelta2, false);
+			ShadeColor4 = GetDeltaColor(ShadeColor3, TargetColorDelta.ColorDelta3, false);
+			return;
+		}
+
+		public void ModifiedColor2() {
+			ShadeColor3 = GetDeltaColor(ShadeColor2, TargetColorDelta.ColorDelta2, false);
+			ShadeColor4 = GetDeltaColor(ShadeColor3, TargetColorDelta.ColorDelta3, false);
+			ShadeColor1 = GetDeltaColor(ShadeColor2, TargetColorDelta.ColorDelta1, true);
+			return;
+		}
+
+		public void ModifiedColor3() {
+			ShadeColor4 = GetDeltaColor(ShadeColor3, TargetColorDelta.ColorDelta3, false);
+			ShadeColor2 = GetDeltaColor(ShadeColor3, TargetColorDelta.ColorDelta2, true);
+			ShadeColor1 = GetDeltaColor(ShadeColor2, TargetColorDelta.ColorDelta1, true);
+			return;
+		}
+
+		public void ModifiedColor4() {
+			ShadeColor3 = GetDeltaColor(ShadeColor4, TargetColorDelta.ColorDelta3, true);
+			ShadeColor2 = GetDeltaColor(ShadeColor3, TargetColorDelta.ColorDelta2, true);
+			ShadeColor1 = GetDeltaColor(ShadeColor2, TargetColorDelta.ColorDelta1, true);
+			return;
+		}
+	}
+}
+#endif
