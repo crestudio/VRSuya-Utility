1e1bc786251d4444e5103a12f51401363ff32caf
Macchiato <simppp@gmail.com>
2026-03-26 13:06:36 +0900

    Global Transform 컴포넌트 추가

---
+++ Assets/Editor/GlobalTransformEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/*
+ * VRSuya Utility
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.utility {
+
+	[CustomEditor(typeof(GlobalTransform))]
+	[CanEditMultipleObjects]
+	public class GlobalTransformEditor : Editor {
+
+		public override void OnInspectorGUI() {
+			GlobalTransform TargetGlobalTransform = (GlobalTransform)target;
+			Transform TargetTransformComponent = TargetGlobalTransform.transform;
+			EditorGUILayout.Space(2f);
+			EditorGUI.BeginChangeCheck();
+			Vector3 CurrentGlobalPosition = TargetTransformComponent.position;
+			Vector3 NewGlobalPosition = EditorGUILayout.Vector3Field("Position", CurrentGlobalPosition);
+			Vector3 CurrentGlobalRotation = TargetTransformComponent.eulerAngles;
+			Vector3 NewGlobalRotation = EditorGUILayout.Vector3Field("Rotation", CurrentGlobalRotation);
+			if (EditorGUI.EndChangeCheck()) {
+				Undo.RecordObject(TargetTransformComponent, "VRSuya Global Transform");
+				TargetTransformComponent.position = NewGlobalPosition;
+				TargetTransformComponent.eulerAngles = NewGlobalRotation;
+				EditorUtility.SetDirty(TargetTransformComponent);
+			}
+			GUI.enabled = false;
+			Vector3 CurrentGlobalScale = TargetTransformComponent.lossyScale;
+			EditorGUILayout.Vector3Field("Scale", CurrentGlobalScale);
+			GUI.enabled = true;
+		}
+	}
+}
---
+++ Assets/Runtime/GlobalTransform.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+/*
+ * VRSuya Utility
+ * Contact : vrsuya@gmail.com // Twitter : https://twitter.com/VRSuya
+ */
+
+namespace com.vrsuya.utility {
+
+	[AddComponentMenu("VRSuya/VRSuya Global Transform")]
+	[RequireComponent(typeof(Transform))]
+	public class GlobalTransform : MonoBehaviour { }
+}
