b4062229c4f291920e05d01de47a9e9c11379b1a
Macchiato <simppp@gmail.com>
2026-04-03 07:55:04 +0900

    네임스페이스 변경에 따른 대응

--- Assets/Runtime/AssetProcessor.cs
+++ Assets/Runtime/AssetProcessor.cs
@@ -8,7 +8,8 @@ using UnityEditor.Animations;
 using UnityEngine;
 
 using VRC.SDK3.Avatars.Components;
-using static com.vrsuya.utility.TextureReplacer;
+
+using static VRSuya.Utility.TextureReplacer;
 
 /*
  * VRSuya Utility
